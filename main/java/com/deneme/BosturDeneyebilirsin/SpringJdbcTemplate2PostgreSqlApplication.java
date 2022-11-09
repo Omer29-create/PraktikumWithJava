07a7315a0f41b86b67fdbb83c2a8646562567dac
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-11-09 14:27:02 +0100

    Update SpringJdbcTemplate2PostgreSqlApplication.java

--- main/java/com/deneme/BosturDeneyebilirsin/SpringJdbcTemplate2PostgreSqlApplication.java
+++ main/java/com/deneme/BosturDeneyebilirsin/SpringJdbcTemplate2PostgreSqlApplication.java
@@ -1,8 +1,5 @@
 package com.deneme.BosturDeneyebilirsin;
 
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
