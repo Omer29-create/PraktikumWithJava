7579e2f5da63b03064e62e8810a59659ff825ae7
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-11-16 23:47:56 +0100

    Update UserRepository.java

--- main/java/com/deneme/BosturDeneyebilirsin/Repository/UserRepository.java
+++ main/java/com/deneme/BosturDeneyebilirsin/Repository/UserRepository.java
@@ -13,14 +13,6 @@ package com.deneme.BosturDeneyebilirsin.Repository;
 
 
 
-
-
-
-
-
-
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
